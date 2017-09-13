29883af94ebf5a4e2269183df34e57a59bee9cab
chyupick <chyupick@gmail.com>
2017-09-13 17:57:11 +0300

    begin App dev

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
+import data from 'products.json';
+
 export default class App extends Component{
     constructor(props) {
         super(props);
+        this.state = {
+            data: null,
+            searchTerm: '',
+            active: 0
+        };
     }
 
-    var products = JSON.parse(fs.readFileSync('../products.json'));
+    products = JSON.parse(fs.readFileSync('../products.json'));
+
+    loadData() {
+        this.setState({})
+    }
 
     render() {
+        return (
+            const photoList = this.products.map(item => <span key={products.photo_list[item].variant}>
+                {products.photo_list[item].large}
+                {products.photo_list[item].thumb}
+                {products.photo_list[item].lowREs}
+                {products.photo_list[item].main}</span>)
+    )
 
     }
 }
